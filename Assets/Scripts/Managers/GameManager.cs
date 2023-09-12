d227fe378aded35b3758d01b0e35c2425c748647
Dohaming <smilebello@gmail.com>
2023-09-12 15:10:42 +0900

    hi2

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -8,12 +8,66 @@ public class GameManager : Singleton<GameManager>
     [SerializeField]
     public Dictionary<string, Sprite> SpriteDic = new Dictionary<string, Sprite>();
     public Sprite sp;
-    public GameObject g;
-    public Transform t;
+    //
+    //생성된 벽돌 갯수
+    private int brickCreate;
+    //부순 벽돌 갯수 (Need Count)
+    private int brickDestroy;
+    public int BrickDestroy
+    {
+        get
+        {
+            brickLeft = brickCreate - brickDestroy;
+            if (brickCreate == brickDestroy)
+            {
+                GameOver();
+            }
+            return brickDestroy;
+        }
+
+        set
+        {
+            brickDestroy = value;
+        }
+    }
+    // 남은 벽돌 갯수 (남은 = 생성 - 부순)
+    private int brickLeft;
+
+    //활동중인 공 갯수 (0이 되면 게임 오버)
+    private int ballCount;
+    public int BallCount
+    {
+        get 
+        { 
+            if (ballCount < 1) 
+            {
+                GameOver();
+            }
+            return ballCount; 
+        }
+
+        set
+        {
+            ballCount = value;
+        }
+    }
+
+    //
+    //벽돌이 삭제될때 사용되는 함수
+    public void BrickTouch()
+    {
+        brickDestroy--;
+    }
+
+    public void GameOver()
+    {
+        //GameOver
+        Time.timeScale = 0;
+    }
+
     public override void Awake()
     {
         base.Awake();
-      
     }
     public void Start()
     {
